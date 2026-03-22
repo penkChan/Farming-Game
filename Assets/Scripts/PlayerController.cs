f3466f5733b990c3a93d032c63884ea527798829
chenpeng <pengcipaci@163.com>
2026-03-22 17:59:52 +0800

    feat: add player move and idle animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed = 8f;
     public InputActionReference moveInput;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        this.rb.velocity = this.moveInput.action.ReadValue<Vector2>() * this.moveSpeed;
-        Debug.Log(this.moveInput.action.ReadValue<Vector2>());
-
+        this.rb.velocity = this.moveInput.action.ReadValue<Vector2>().normalized * this.moveSpeed;
+        this.animator.SetFloat("speed", this.rb.velocity.magnitude);
+        if (this.rb.velocity.x > 0) {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        } else if (this.rb.velocity.x < 0) {
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
